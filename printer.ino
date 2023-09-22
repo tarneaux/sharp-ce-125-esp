bee8a9a4f6e89e0cdbd5cf5f728ee47da67d0869
tarneo <tarneo@tarneo.fr>
2023-09-22 15:36:18 +0200

    Add wifi functionality

--- printer.ino
+++ printer.ino
@@ -5,18 +5,38 @@
 
 #include <string>
 #include <vector>
+#include "WiFi.h"
+
+#include "secrets.h"
 #include "printer-lib.h"
+
+WiFiServer server(80);
+Printer printer(D0, D1, D3, D2);
 
 using namespace std;
 
 void setup() {
-    Printer printer(D0, D1, D3, D2);
-
-    string toprint = "This is a test of the printer.\nAnd this is another line.";
-
-    printer.Print(toprint);
+	WiFi.begin(SECRET_SSID, SECRET_PASS);
+	while (WiFi.status() != WL_CONNECTED) {}
+	Serial.begin(9600);
+	Serial.println("Connected to WiFi");
+	Serial.println(WiFi.localIP());
+	server.begin();
 }
 
 void loop() {
+	// See if we have a new client
+	WiFiClient client = server.available();
+	if (client) {
+		// Start the socket communication.
+		// Get as much lines as there are and print them.
+		while (client.connected()) {
+			if (client.available()) {
+				String line = client.readStringUntil('\n');
+				printer.Print(line.c_str());
+				client.print("OK\r\n");
+			}
+		}
+	}
 }
 
